7fb9288e73d077e5e9de2457eb180dcb837d13ee
JesusPaucar <42385952+JesusPaucar@users.noreply.github.com>
2022-10-24 15:34:38 -0500

    Update WebPage.py

--- WebPage.py
+++ WebPage.py
@@ -14,9 +14,9 @@ with tab1:
   st.header('1. Nosotros', anchor = None)
   st.write('uno dos tres cuatro cinco seis siete ocho nueve diez.... añadir descripción de nosotros')
   image = io.imread('Image_Lab.jpg')
-  col1, col2, col3 = st.columns(3)
-  col2.image(image, width = 1000)
-  #st.image(image, caption = 'Imagen de Prueba', width = None, use_column_width = True, clamp = False, channels = 'RGB', output_format = 'jpg')
+  #col1, col2, col3 = st.columns(3)
+  #col2.image(image, width = 1000)
+  st.image(image, caption = 'Imagen de Prueba', width = None, use_column_width = True, clamp = False, channels = 'RGB', output_format = 'jpg')
 with tab2:
   st.markdown('**Miembros**')
 with tab3:
