34d87da2e06f2ed83b73444de8683d6eaaa35d64
JesusPaucar <42385952+JesusPaucar@users.noreply.github.com>
2022-10-24 14:44:26 -0500

    Update WebPage.py

--- WebPage.py
+++ WebPage.py
@@ -8,17 +8,18 @@ def download_data():
   gdown.download(url, output, quiet = False)
 
 download_data()
-tab1, tab2, tab3, tab4 = st.tabs(['Descripción', 'Miembros', 'Solicitudes', 'Contacto'])
+tab1, tab2, tab3, tab4 = st.tabs(['Nosotros', 'Miembros', 'Solicitudes', 'Contacto'])
 with tab1:
-  st.title('Bienvenidos al Laboratorio de Ingeniería UPCH')
-  st.header('1. Descripción', anchor = None)
+  st.title('Bienvenidos a los Laboratorios de Ingeniería UPCH')
+  st.header('1. Nosotros', anchor = None)
+  st.text('uno dos tres cuatro cinco seis siete ocho nueve diez.... añadir descripción de nosotros')
   image = io.imread('Image_Lab.jpg')
   st.image(image, caption = 'Imagen de Prueba', width = None, use_column_width = None, clamp = False, channels = 'RGB', output_format = 'jpg')
 with tab2:
   st.markdown('**Miembros**')
 with tab3:
-  st.markdown('# Solicitudes', unsafe_allow_html = True)
-  st.markdown('#### Solicitar Materiales', unsafe_allow_html = True)
-  st.markdown('#### Solicitud de copias', unsafe_allow_html = True)
+  st.markdown('# Solicitudes', unsafe_allow_html = False)
+  st.markdown('#### Solicitar Materiales', unsafe_allow_html = False)
+  st.markdown('#### Solicitud de copias', unsafe_allow_html = False)
 with tab4:
   st.markdown('**Teléfonos de contacto**')
