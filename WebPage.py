370a27de4f366950afe28d8820404cdd2f110d56
JesusPaucar <42385952+JesusPaucar@users.noreply.github.com>
2022-10-24 12:31:30 -0500

    Update WebPage.py

--- WebPage.py
+++ WebPage.py
@@ -3,8 +3,6 @@ from skimage import io
 #from skimage.io import imread
 import gdown
 
-import streamlit as st
-
 # Using object notation
 add_selectbox = st.sidebar.selectbox(
     "How would you like to be contacted?",
@@ -17,18 +15,17 @@ with st.sidebar:
         "Choose a shipping method",
         ("Standard (5-15 days)", "Express (2-5 days)")
     )
-'''
-def download_data():
-  url = "https://drive.google.com/uc?id=1pi1tDrxSxin4v6Vg58R0wVHPOC--xIgq"
-  output = 'Image_Lab.jpg'
-  gdown.download(url, output, quiet = False)
+    
+#def download_data():
+#  url = "https://drive.google.com/uc?id=1pi1tDrxSxin4v6Vg58R0wVHPOC--xIgq"
+#  output = 'Image_Lab.jpg'
+#  gdown.download(url, output, quiet = False)
 
-download_data()
-st.title('Bienvenidos al Laboratorio de Ingeniería UPCH')
-st.header('1. Descripción', anchor = None)
-image = io.imread('Image_Lab.jpg')
-st.image(image, caption = 'Imagen de Prueba', width = None, use_column_width = None, clamp = False, channels = 'RGB', output_format = 'jpg')
-st.markdown('# Solicitudes', unsafe_allow_html = True)
-st.markdown('#### Solicitar Materiales', unsafe_allow_html = True)
-st.markdown('#### Solicitud de copias', unsafe_allow_html = True)
-'''
+#download_data()
+#st.title('Bienvenidos al Laboratorio de Ingeniería UPCH')
+#st.header('1. Descripción', anchor = None)
+#image = io.imread('Image_Lab.jpg')
+#st.image(image, caption = 'Imagen de Prueba', width = None, use_column_width = None, clamp = False, channels = 'RGB', output_format = 'jpg')
+#st.markdown('# Solicitudes', unsafe_allow_html = True)
+#st.markdown('#### Solicitar Materiales', unsafe_allow_html = True)
+#st.markdown('#### Solicitud de copias', unsafe_allow_html = True)
