f39fe2032d1e85a0dad7eaf750dc771b6c099033
JesusPaucar <42385952+JesusPaucar@users.noreply.github.com>
2022-10-24 09:58:47 -0500

    Update WebPage.py

--- WebPage.py
+++ WebPage.py
@@ -4,11 +4,11 @@ from skimage import io
 import gdown
 
 def download_data():
-  url = "https://drive.google.com/uc?id=1JHmFyGpUnsn0fnAFuwHC1UuUoTm-ge3C"
-  output = 'Lena.jpg'
+  url = "https://drive.google.com/uc?id=1pi1tDrxSxin4v6Vg58R0wVHPOC--xIgq"
+  output = 'Image_Lab.jpg'
   gdown.download(url, output, quiet = False)
 
 download_data()
 st.title('Bienvenidos al Laboratorio de Ingeniería UPCH')
-image = io.imread('Lena.jpg')
+image = io.imread('Image_Lab.jpg')
 st.image(image, caption = 'Imagen de Prueba', width = None, use_column_width = None, clamp = False, channels = 'RGB', output_format = 'jpg')
