280d75b2982c2266675ff236ca2dd1b94b030104
JesusPaucar <42385952+JesusPaucar@users.noreply.github.com>
2022-10-24 16:49:31 -0500

    Update WebPage.py

--- WebPage.py
+++ WebPage.py
@@ -2,12 +2,42 @@ import streamlit as st
 from skimage import io
 import gdown
 
-def download_data():
-  url = "https://drive.google.com/uc?id=1pi1tDrxSxin4v6Vg58R0wVHPOC--xIgq"
-  output = 'Image_Lab.jpg'
+def download_data(url, output):
   gdown.download(url, output, quiet = False)
 
-download_data()
+# Imagen para la presentación
+url1 = "https://drive.google.com/uc?id=1pi1tDrxSxin4v6Vg58R0wVHPOC--xIgq"
+output1 = 'Image_Lab.jpg'
+
+# Imagen para miembros (Parte superior)
+url2 = "https://drive.google.com/uc?id=11v-Ys6Ll30ZMze6fJLkS_qyM9od-3rl6"
+output2 = 'Fig_M.jpg'
+
+# Imagen para miembros (Parte inferior 1)
+url3 = "https://drive.google.com/uc?id=1cSiWY4jlXls4FsEo12geg_lDN_9xdrb-"
+output3 = 'Fig_1.jpg'
+
+# Imagen para miembros (Parte inferior 2)
+url4 = "https://drive.google.com/uc?id=1y72UA5dOzAu3s-3s8vF6JMwvhJNAa2Vc"
+output4 = 'Fig_2.jpg'
+
+
+# Imagen para miembros (Parte inferior 3)
+url5 = "https://drive.google.com/uc?id=1Hmf8YUi5O1gDQ-ejPWTI7xgYC0n9DocP"
+output5 = 'Fig_3.jpg'
+
+
+# Imagen para miembros (Parte inferior 4)
+url6 = "https://drive.google.com/uc?id=1kZsL14HqU_no4zOxqsPLIzAb7kQMOpUd"
+output6 = 'Fig_4.jpg'
+
+download_data(url1, output1)
+download_data(url2, output2)
+download_data(url3, output3)
+download_data(url4, output4)
+download_data(url5, output5)
+download_data(url6, output6)
+
 tab1, tab2, tab3, tab4 = st.tabs(['Nosotros', 'Miembros', 'Solicitudes', 'Contacto'])
 with tab1:
   st.title('Bienvenidos a los Laboratorios de Ingeniería UPCH')
@@ -19,6 +49,19 @@ with tab1:
   st.image(image, caption = 'Imagen de Prueba', width = None, use_column_width = True, clamp = False, channels = 'RGB', output_format = 'jpg')
 with tab2:
   st.markdown('**Miembros**')
+  image1 = io.imread('Fig_M.jpg')
+  image2 = io.imread('Fig_1.jpg')
+  image3 = io.imread('Fig_2.jpg')
+  image4 = io.imread('Fig_3.jpg')
+  image5 = io.imread('Fig_4.jpg')
+  
+  col1, col2, col3 = st.columns(3)
+  col2.image(image1)
+  col11, col22, col33, col44 = st.columns(4)
+  col11.image(image2, caption = 'P1')
+  col22.image(image3)
+  col33.image(image4)
+  col44.image(image5)
 with tab3:
   st.markdown('# Solicitudes', unsafe_allow_html = False)
   st.markdown('#### Solicitar Materiales', unsafe_allow_html = False)
