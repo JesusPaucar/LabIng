faf76492f849e68b1f93cf1d1a92631fb5b12af7
JesusPaucar <42385952+JesusPaucar@users.noreply.github.com>
2022-10-25 11:20:35 -0500

    Update WebPage.py

--- WebPage.py
+++ WebPage.py
@@ -2,12 +2,76 @@ import streamlit as st
 from skimage import io
 import gdown
 #from streamlit.componets.v1 import html
-#from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
-#from htbuilder.units import percent, px
-#from htbuilder.funcs import rgba, rgb
+from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
+from htbuilder.units import percent, px
+from htbuilder.funcs import rgba, rgb
 
 def download_data(url, output):
   gdown.download(url, output, quiet = False)
+
+def image(src_as_string, **style):
+  return img(src = src_as_string, style = styles(**style))
+
+def link(link, text, **style):
+  return a(_href = link, _target = 'blank', style = styles(**style))(text)
+
+def layout(*args):
+  
+  style = """
+  <style>
+    # MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    .stApp {bottom: 105px; }
+  </style>
+  """
+  
+  style_div = styles(
+    position = 'fixed',
+    left = 0,
+    bottom = 0,
+    margin = px(0, 0, 0, 0),
+    width = percent(100),
+    color = "black",
+    text_align = "center",
+    height = "auto",
+    opacity = 1)
+  
+  style_hr = styles(
+    display = "block",
+    margin = px(8, 8, "auto", "auto"),
+    border_style = "inset",
+    border_width = px(2)
+  )
+  
+  body = p()
+  foot = div(
+    style = style_div
+  )(
+    hr(
+      style = style_hr
+    ),
+    body
+  )
+  st.markdown(style, unsafe_allow_html = True)
+  
+  for arg in args:
+    if isintance(arg, str):
+      body(arg)
+    elif isintance(arg, HtmlElement):
+      body(arg)
+  st.markdown(str(foot), unsafe_allow_html = True)
+
+def footer():
+  myargs = [
+    "Made in ",
+    image('https://avatars3.githubusercontent.com/u/45109972?s=400&v=4',
+          width = px(25), height = px(25)),
+    "with ❤️ by",
+    link("https://twitter.com/ChristianKlose3", "@zafiro"),
+    br(),
+    link("https://upch.edu.pe", image('https://i.imgur.com/thJhzOO.png')),
+  ]
+ layout(*myargs)
 
 # Imagen para la presentación
 url1 = "https://drive.google.com/uc?id=1pi1tDrxSxin4v6Vg58R0wVHPOC--xIgq"
@@ -56,6 +120,7 @@ with tab1:
   st.header('4. Visión', anchor = None)
   st.header('5. Campus', anchor = None)
   st.header('6. Organización', anchor = None)
+  footer()
 with tab2:
   st.markdown('**Miembros**')
   image1 = io.imread('Fig_M.jpg')
