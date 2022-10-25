2eea66836926decb69928fa93a2d9383fcc3bf1a
JesusPaucar <42385952+JesusPaucar@users.noreply.github.com>
2022-10-25 11:24:56 -0500

    Update WebPage.py

--- WebPage.py
+++ WebPage.py
@@ -64,12 +64,12 @@ def layout(*args):
 def footer():
   myargs = [
     "Made in ",
-    image('https://avatars3.githubusercontent.com/u/45109972?s=400&v=4',
+    image('upch.jpg',
           width = px(25), height = px(25)),
     "with ❤️ by",
-    link("https://twitter.com/ChristianKlose3", "@zafiro"),
+    link("https://", "@zafiro"),
     br(),
-    link("https://upch.edu.pe", image('https://i.imgur.com/thJhzOO.png')),
+    link("https://upch.edu.pe", image('Fig_1.jpg')),
   ]
   layout(*myargs)
 
